53d9af4ff0fa0212232b26c191c9048bb94c7ae4
Wilson <wilsonvargas_6@outlook.com>
2017-10-03 19:33:35 -0500

    Added suppor to UWP platform

--- src/Screenshot/Plugin.Screenshot.UWP/ScreenshotImplementation.cs
+++ src/Screenshot/Plugin.Screenshot.UWP/ScreenshotImplementation.cs
@@ -1,6 +1,13 @@
 using Plugin.Screenshot.Abstractions;
 using System;
+using System.IO;
 using System.Threading.Tasks;
+using Windows.Graphics.Display;
+using Windows.Graphics.Imaging;
+using Windows.Security.Cryptography;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media.Imaging;
 
 namespace Plugin.Screenshot
 {
@@ -9,9 +16,36 @@ namespace Plugin.Screenshot
     /// </summary>
     public class ScreenshotImplementation : IScreenshot
     {
-        public Task<byte[]> CaptureAsync()
+        public async Task<byte[]> CaptureAsync()
         {
-            throw new NotImplementedException();
+            var rtb = new RenderTargetBitmap();
+            await rtb.RenderAsync(Window.Current.Content);
+
+            var pixelBuffer = await rtb.GetPixelsAsync();
+            byte[] pixels;
+            CryptographicBuffer.CopyToByteArray(pixelBuffer, out pixels);
+
+            // Useful for rendering in the correct DPI
+            var displayInformation = DisplayInformation.GetForCurrentView();
+
+            var stream = new InMemoryRandomAccessStream();
+            var encoder = await BitmapEncoder.CreateAsync(BitmapEncoder.JpegEncoderId, stream);
+            encoder.SetPixelData(BitmapPixelFormat.Bgra8,
+            BitmapAlphaMode.Premultiplied,
+            (uint)rtb.PixelWidth,
+            (uint)rtb.PixelHeight,
+            displayInformation.RawDpiX,
+            displayInformation.RawDpiY,
+            pixels);
+
+            await encoder.FlushAsync();
+            stream.Seek(0);
+
+            var readStram = stream.AsStreamForRead();
+            var bytes = new byte[readStram.Length];
+            readStram.Read(bytes, 0, bytes.Length);
+
+            return bytes;
         }
     }
 }

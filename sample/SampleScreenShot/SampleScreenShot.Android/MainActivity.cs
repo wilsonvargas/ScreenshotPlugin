a29bc4bbf65932552558ef8b59b7b097e30c3106
Wilson <wilsonvargas_6@outlook.com>
2017-10-07 20:06:44 -0500

    Add sample using local nuget package.

---
+++ sample/SampleScreenShot/SampleScreenShot.Android/MainActivity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+namespace SampleScreenShot.Droid
+{
+    [Activity(Label = "SampleScreenShot", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            Forms.Init(this, bundle);
+            LoadApplication(new App());
+        }
+    }
+}
+
---
+++ sample/SampleScreenShot/SampleScreenShot/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Plugin.Screenshot;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SampleScreenShot
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            var stream1 = new MemoryStream(await CrossScreenshot.Current.CaptureAsync());
+            IncidentImageData.Source = ImageSource.FromStream(() => stream1);
+        }
+
+        private async void Button_Clicked_1(object sender, EventArgs e)
+        {
+            try
+            {
+                await CrossScreenshot.Current.CaptureAndSaveAsync();
+                label.Text = DateTime.Now.ToString();
+            }
+            catch (Exception ex)
+            {
+                await App.Current.MainPage.DisplayAlert("Error", ex.Message, "ok");
+            }
+            
+        }
+    }
+}

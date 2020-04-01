f4f70a73b2cc0df2b7b9f5fffb74645a955826d6
Wilson Vargas <wilsonvargas_6@outlook.com>
2020-04-01 17:20:37 -0500

    update sample

---
+++ src/Sample/Sample/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Plugin.Screenshot;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Sample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            var stream = new MemoryStream(await CrossScreenshot.Current.CaptureAsync());
+            ImageData.Source = ImageSource.FromStream(() => stream);
+        }
+
+        private async void Button_Clicked_1(object sender, EventArgs e)
+        {
+            try
+            {
+                string path = await CrossScreenshot.Current.CaptureAndSaveAsync();
+                label.Text = "Location: " + path;
+            }
+            catch (Exception ex)
+            {
+                await App.Current.MainPage.DisplayAlert("Error", ex.Message, "ok");
+            }
+        }
+    }
+}

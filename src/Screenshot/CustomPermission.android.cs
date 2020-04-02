a006e94465593c0a92b54273ff7441bb62c373be
Wilson Vargas <wilsonvargas_6@outlook.com>
2020-04-01 19:48:49 -0500

    Adding Xamarin Essentials for manage permissions

---
+++ src/Screenshot/CustomPermission.android.cs
@@ -0,0 +1,15 @@
+﻿using Android;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static Xamarin.Essentials.Permissions;
+
+namespace Plugin.Screenshot
+{
+    public class StoragePermission : BasePlatformPermission
+    {
+        public override (string androidPermission, bool isRuntime)[] RequiredPermissions =>
+                new (string, bool)[] { (Manifest.Permission.WriteExternalStorage, true),
+                (Manifest.Permission.ReadExternalStorage, true)};
+    }
+}
--- src/Screenshot/Screenshot.android.cs
+++ src/Screenshot/Screenshot.android.cs
@@ -1,10 +1,13 @@
-﻿using Android.App;
+﻿using Android;
+using Android.App;
 using Android.Graphics;
-using Plugin.CurrentActivity;
+using Android.OS;
 using System;
 using System.IO;
 using System.Threading.Tasks;
 using Xamarin.Essentials;
+using static Xamarin.Essentials.Permissions;
+using PermissionStatus = Xamarin.Essentials.PermissionStatus;
 
 namespace Plugin.Screenshot
 {
@@ -17,12 +20,17 @@ namespace Plugin.Screenshot
 
         public async Task<string> CaptureAndSaveAsync()
         {
+            if (!(await RequestStoragePermission()))
+            {
+                var missingPermissions = string.Join(", ", nameof(StoragePermission));
+                throw new Exception($"{missingPermissions} permission(s) are required.");
+            }
             var bytes = await CaptureAsync();
-            Java.IO.File picturesFolder = Android.OS.Environment.GetExternalStoragePublicDirectory(Android.OS.Environment.DirectoryPictures);
+            Java.IO.File picturesFolder = Android.OS.Environment.GetExternalStoragePublicDirectory(Android.OS.Environment.DirectoryDcim);
             string date = DateTime.Now.ToString().Replace("/", "-").Replace(":", "-");
             try
             {
-                string filePath = System.IO.Path.Combine(picturesFolder.AbsolutePath, "Screnshot-" + date + ".png");
+                string filePath = System.IO.Path.Combine(picturesFolder.AbsolutePath + "/Camera", "Screnshot-" + date + ".png");
                 using (System.IO.FileStream SourceStream = System.IO.File.Open(filePath, System.IO.FileMode.OpenOrCreate))
                 {
                     SourceStream.Seek(0, System.IO.SeekOrigin.End);
@@ -41,7 +49,7 @@ namespace Plugin.Screenshot
             await Task.Delay(1000);
             if (Context == null)
             {
-                throw new Exception("You have to set Screenshot.Activity in your Android project");
+                throw new Exception("You have to set Screenshot. Activity in your Android project");
             }
             var view = Context.Window.DecorView;
             view.DrawingCacheEnabled = true;
@@ -57,6 +65,26 @@ namespace Plugin.Screenshot
             }
             view.DrawingCacheEnabled = false;
             return bitmapData;
+        }
+
+        async Task<bool> RequestStoragePermission()
+        {
+            //We always have permission on anything lower than marshmallow.
+            if ((int)Build.VERSION.SdkInt < 23)
+                return true;
+
+            var status = await CheckStatusAsync<StoragePermission>();
+            if (status != PermissionStatus.Granted)
+            {
+                Console.WriteLine("Does not have storage permission granted, requesting.");
+                var result = await RequestAsync<StoragePermission>();
+                if (result != PermissionStatus.Granted)
+                {
+                    Console.WriteLine("Storage permission Denied.");
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
